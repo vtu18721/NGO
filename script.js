1911c4df319e76a30464d2bdc2beb0d020aef4d2
vtu18721 <103377829+vtu18721@users.noreply.github.com>
2023-07-11 20:13:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+  
+    // You can add code here to send the form data to the server or display a success message
+    alert('Thank you for contacting us! We will get back to you soon.');
+  });
